50826af2253dd7e4e55ecfb9caf88c2c276d2664
ariel041596 <chocokises17@gmail.com>
2021-04-14 20:16:27 +0800

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to the ShopCenter</h1>
+    </>
+  );
+};
+
+export default App;
+
+// rm -rf .git to remove the git repository of the current folder
