4483aa615379e6a7bc8787495cd3ee4327c05d70
ariel041596 <chocokises17@gmail.com>
2021-04-15 15:48:32 +0800

    Added concurrently

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,30 @@
+// React
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// Bootstrap
+import { Container } from "react-bootstrap";
+
+// Components
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
+// Screens
+import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
+
 const App = () => {
   return (
-    <>
-      <h1>Welcome to the ShopCenter</h1>
-    </>
+    <Router>
+      <Header></Header>
+      <main className="py-3">
+        <Container>
+          <Route path="/" component={HomeScreen} exact></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+        </Container>
+      </main>
+      <Footer></Footer>
+    </Router>
   );
 };
 
